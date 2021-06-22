08f4242727166a4ada288c1600e98e07ce5a6440
Ann 杏 Kilzer <akilzer@gmail.com>
2021-06-22 10:32:00 +0900

    I18n tooling (#33)
    * add python file for managing translations
    
    * alphabetize everything
    
    * add requirements
    
    * start writing tests, move to a subfolder
    
    * update paths
    
    * update test case
    
    * more testing
    
    * more tests and simplify logic
    
    * small fixes

---
+++ tools/test_translation_manager.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from translation_manager import *
+
+class TestTranslationManager(unittest.TestCase):
+
+    def test_copy_new_keys_to_locale(self):
+        """A larger test with varying structure"""
+        primaryDict = {
+            "one": {
+                "whatever": {"foo": "bar"},
+                "okay": {"hoge": "hoge"}
+            },
+            "two": "top level",
+            "three": {
+                "rhubarb": { "eh": "eeeh"},
+                "teeth": "yes"
+            }
+        }
+
+        secondaryDict = {
+            "one": {"okay": {"hoge": "ほげ"}},
+            "three": {
+                "teeth": "はい"
+            }
+        }
+
+        copy_new_keys_to_locale(primaryDict, secondaryDict)
+
+        expectedDict = {
+            "one": {
+                "whatever": {"foo": "bar"},
+                "okay": {"hoge": "ほげ"}
+            },
+            "two": "top level",
+            "three": {
+                "rhubarb": { "eh": "eeeh"},
+                "teeth": "はい"
+            }
+        }
+        self.assertEqual(write_json(expectedDict), write_json(secondaryDict))
+
+        
+    def test_copy_new_keys_to_locale_string_to_obj(self):
+        """Tests changing a string into an object"""
+        primaryDict = {
+            "one": {"hoge": "hoge"}
+        }
+
+        secondaryDict = {
+            "one": "was a string"
+        }
+
+        copy_new_keys_to_locale(primaryDict, secondaryDict)
+
+        expectedDict = {
+            "one": {"hoge": "hoge"}
+        }
+        self.assertEqual(write_json(expectedDict), write_json(secondaryDict))
+
+
+    def test_copy_new_keys_to_locale_obj_to_string(self):
+        """Tests changing an object to a string"""
+        primaryDict = {
+            "one": "is a string"
+        }
+
+        secondaryDict = {
+            "one": {"hoge": "hoge"}
+        }
+
+        copy_new_keys_to_locale(primaryDict, secondaryDict)
+
+        expectedDict = {
+            "one": "is a string"
+        }
+        self.assertEqual(write_json(expectedDict), write_json(secondaryDict))
+
+        
+    def test_copy_new_keys_to_locale_expand_child(self):
+        """Tests adding to child object"""
+        primaryDict = {
+            "one": {"hoge": "hoge",
+                    "naruhodo": "naruhodo"}
+        }
+
+        secondaryDict = {
+            "one": {"hoge": "ほげ"}
+        }
+
+        copy_new_keys_to_locale(primaryDict, secondaryDict)
+
+        expectedDict = {
+            "one": {"hoge": "ほげ",
+                    "naruhodo": "naruhodo"}
+        }
+        self.assertEqual(write_json(expectedDict), write_json(secondaryDict))
+
+
+    def test_copy_new_keys_to_locale_no_changes(self):
+        """Tests running on no changes"""
+        primaryDict = {
+            "one": {"hoge": "hoge",
+                    "naruhodo": "naruhodo"}
+        }
+
+        secondaryDict = {
+            "one": {"hoge": "ほげ",
+                    "naruhodo": "なるほど"}
+        }
+
+        copy_new_keys_to_locale(primaryDict, secondaryDict)
+
+        expectedDict = {
+            "one": {"hoge": "ほげ",
+                    "naruhodo": "なるほど"}
+        }
+        self.assertEqual(write_json(expectedDict), write_json(secondaryDict))
+
+                         
+if __name__ == '__main__':
+    unittest.main()
---
+++ tools/translation_manager.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import fire
+import json, copy, glob
+
+LOCALE_DIR="../locales"
+
+INDENT=4
+
+
+def openLocaleFile(fname):
+    """Opens a locale file"""
+    with open(fname) as json_file: 
+      return json.load(json_file) 
+
+  
+def write_json(data):
+    return json.dumps(data, indent=INDENT, ensure_ascii=False, sort_keys=True) 
+
+
+def export(data, fname):
+    """Writes data to the correct locale file"""
+    with open(fname, 'w', encoding='utf-8') as json_file: 
+        json_file.write(write_json(data)) 
+
+        
+def is_expected(key, dest, expValue):
+    return key in dest and isinstance(dest[key], expValue)
+
+        
+def copy_new_keys_to_locale(sourceDict, destDict):
+    """Copy all the keys in sourceDict missing from the destination JSON"""
+    queue = [(sourceDict, destDict)]
+    while len(queue) > 0:
+        (dict1, dict2) = queue.pop(0) 
+        for k, v in dict1.items():
+            if isinstance(v, str):
+                if not is_expected(k, dict2, str):
+                    dict2[k] = copy.deepcopy(v)
+            elif isinstance(v, dict):
+                if is_expected(k, dict2, dict):
+                    # add the subdict to the queue
+                    queue.append((v, dict2[k]))
+                else:
+                    dict2[k] = copy.deepcopy(v)
+
+    
+def get_locale_files(path=LOCALE_DIR):
+
+    return glob.glob(f'{path}/*.json')
+
+
+class TranslationManager(object):
+
+  def alphabetize(self):
+      """Rewrites JSON files to be alphabetized"""
+      for fname in get_locale_files():
+          print(f"Updating {fname}...")
+          data = openLocaleFile(fname)
+          export(data, fname)
+      print("Done!🙌🏻")
+          
+  
+  def copy_new_keys(self, locale="en"):
+    """Moves all missing keys from the primary locale (default is English) to the other locale files"""
+    primaryFName = f"{LOCALE_DIR}/{locale}.json"
+    primaryDict = openLocaleFile(primaryFName)
+
+    for fname in get_locale_files():
+        if fname != primaryFName:
+            print(f"Updating {fname}...")
+            destDict = openLocaleFile(fname)
+            copy_new_keys_to_locale(primaryDict, destDict)
+            export(destDict, fname)
+            
+    print("Done!🙌🏻")
+
+    
+if __name__ == '__main__':
+  fire.Fire(TranslationManager)
